c7a94c886fd9f60aa6e3255adf2613611b9acc12
CharlieGarcia <ing.charlie.garcia@gmail.com>
2016-06-09 22:22:37 -0400

    Initial commit

---
+++ Grades/GradeStatistics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class GradeStatistics
+    {
+        private float averageGrade;
+        private float highestGrade;
+        private float lowestGrade;
+
+        public float AverageGrade { get; set; }
+        public float HighestGrade { get; set; }
+        public float LowestGrade { get; set; }
+    }
+}
